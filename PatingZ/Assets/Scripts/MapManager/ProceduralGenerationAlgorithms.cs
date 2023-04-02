5024d83aa5175b921d5ba8f3467a2b114c5e8620
MallangBag <dlxotkddl1@naver.com>
2023-04-02 14:21:48 +0900

    Update Partitioning

--- PatingZ/Assets/Scripts/MapManager/ProceduralGenerationAlgorithms.cs
+++ PatingZ/Assets/Scripts/MapManager/ProceduralGenerationAlgorithms.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public static class ProceduralGenerationAlgorithms
 {
@@ -47,10 +49,10 @@ public static class ProceduralGenerationAlgorithms
                 if(Random.value < 0.5f)
                 {
                     if(room.size.y >= minHeight * 2){
-                        SplitHorizontally(minWidth, minHeight, roomsQueue, room);
+                        SplitHorizontally(minHeight, roomsQueue, room);
                     }
                     else if(room.size.x >= minWidth * 2){
-                        SplitVertically(minWidth, minHeight, roomsQueue, room);
+                        SplitVertically(minWidth, roomsQueue, room);
                     }
                     else if(room.size.x >= minWidth && room.size.y > minHeight){
                         roomsList.Add(room);
@@ -60,11 +62,11 @@ public static class ProceduralGenerationAlgorithms
                 {
                     if (room.size.x >= minWidth * 2)
                     {
-                        SplitVertically(minWidth, minHeight, roomsQueue, room);
+                        SplitVertically(minWidth, roomsQueue, room);
                     }
                     else if (room.size.y >= minHeight * 2)
                     {
-                        SplitHorizontally(minWidth, minHeight, roomsQueue, room);
+                        SplitHorizontally(minHeight, roomsQueue, room);
                     }
                     else if (room.size.x >= minWidth && room.size.y > minHeight)
                     {
@@ -76,14 +78,24 @@ public static class ProceduralGenerationAlgorithms
         return roomsList;
     }
 
-    private static void SplitVertically(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    private static void SplitVertically(int minWidth, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
-        throw new System.NotImplementedException();
+        var xSplit = Random.Range(1, room.size.x);  //Range (int, int)¸é min <= value < maxÀÓ
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.size.y, room.size.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x + xSplit, room.min.y, room.min.z),
+            new Vector3Int(room.size.x - xSplit, room.size.y, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
     }
 
-    private static void SplitHorizontally(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    private static void SplitHorizontally(int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
-        throw new System.NotImplementedException();
+        var ySplit = Random.Range(1, room.size.y);  //(minHeight, room.size.y - minHeight)
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(room.size.x, ySplit, room.size.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x, room.min.y + ySplit, room.min.z),
+            new Vector3Int(room.size.x, room.size.y - ySplit, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
     }
 }
 
